7d866ff352ac5fd1f647a268887b817d9d2a0c23
akotikov <a.kotikov@flexsoft.com>
2015-11-11 10:10:36 +0600

    Initial revision

---
+++ scripts/DataForce/rpms.sh
@@ -0,0 +1,11 @@
+yum install oracle-rdbms-server-11gR2-preinstall-1.0-3.el7.x86_64
+yum install libgcc-4.8.3-9.el7.*
+yum install libgcc-4.8.3-9.el7.i686
+yum install nss-softokn-freebl-3.16.2.3-9.el7.i686
+yum install glibc-2.17-78.0.1.el7.i686
+yum install compat-libstdc++-33-3.2.3-72.el7.i686
+yum install libstdc++-4.8.3-9.el7.i686
+yum install elfutils-libelf-0.160-1.el7.i686
+yum install elfutils-libelf-devel-0.160-1.el7.i686
+yum install elfutils-libelf-devel-0.160-1.el7.x86_64
+yum install mksh.x86_64
